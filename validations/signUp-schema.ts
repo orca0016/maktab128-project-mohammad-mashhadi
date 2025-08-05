1869071701882d961a168eda04155b19b26b8dc2
Mohammad m <mashhadim901@gmail.com>
2025-08-05 14:09:33 +0430

    fix:change stronger validation password for login and sign up

--- validations/signUp-schema.ts
+++ validations/signUp-schema.ts
@@ -1,16 +1,19 @@
 import * as z from "zod";
 
+const strongPasswordSchema = z
+  .string()
+  .min(8, "حداقل طول رمز ورود باید ۸ کاراکتر باشد .")
+  .regex(/[A-Z]/, "رمز ورود باید یک حرف بزرگ انگلیسی داشته باشد ")
+  .regex(/[a-z]/, "رمز ورود باید حداقل یک حرف کوچک انگلیسی داشته باشد .")
+  .regex(/[0-9]/, "رمز ورود باید شامل عدد باشد")
+  .regex(/[^A-Za-z0-9]/, "رمز ورود باید حداقل شامل یک حرف خاص باشد .($#@%.)");
 export const signUpFormSchema = z.object({
   username: z
     .string({ error: "نام کاربری اجباری است." })
     .trim()
     .min(4, { error: "حداقل طول نام کاربری ۴ کاراکتر است ." })
     .max(10, { error: "حداکثر طول نام کاربری ۱۰ کاراکتر است." }),
-  password: z
-    .string({ error: "رمز عبور مورد نیاز است ." })
-    .trim()
-    .min(6, { error: "رمز عبور باید بیشتر از ۶ کاراکتر باشد ." })
-    .max(14, { error: "حداکثر طول رمز عبور ۱۴ کاراکتر است ." }),
+  password: strongPasswordSchema,
   firstname: z
     .string({ error: "اسم   اجباری است." })
     .trim()
