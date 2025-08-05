addca4f39dad47003491cffd1569e0602b43edbe
Mohammad m <mashhadim901@gmail.com>
2025-08-05 14:03:53 +0430

    feat:add api route for sign up page

---
+++ app/api/(auth)/signUp/route.ts
@@ -0,0 +1,49 @@
+import { cookies } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { username, password, firstname, lastname, phoneNumber, address } =
+      await request.json();
+    const res = await fetch("http://localhost:8000/api/auth/signup", {
+      body: JSON.stringify({
+        username,
+        password,
+        firstname,
+        lastname,
+        phoneNumber,
+        address,
+      }),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const userData: IResponseUserData = await res.json();
+
+    const cookieStore = await cookies();
+    cookieStore.set({
+      name: "access-token",
+      value: userData.token.accessToken,
+      httpOnly: true,
+      path: "/",
+    });
+    cookieStore.set({
+      name: "refresh-token",
+      value: userData.token.refreshToken,
+      httpOnly: true,
+      path: "/",
+    });
+
+    return NextResponse.json(
+      { message: "user successfully created." },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "something vents wrong" + error },
+      { status: 401 }
+    );
+  }
+}
