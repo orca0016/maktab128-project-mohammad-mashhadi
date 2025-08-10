7a7318a1d17ee5d4e808586065330b66421faaaa
Mohammad m <mashhadim901@gmail.com>
2025-08-10 15:24:36 +0430

    feat:handling logout logic

--- components/molecules/authorize-user-action.tsx
+++ components/molecules/authorize-user-action.tsx
@@ -1,30 +1,42 @@
 import { cookies } from "next/headers";
 import UserAccessMenu from "../atoms/user-access-menu";
-const AuthorizeUserAction = async () => {
-  const cookieStore = await cookies();
-  const userDetail: { id?: string; role?: string } = await fetch(
-    "http://localhost:8000/api/users/me",
-    {
-      headers: {
-        Authorization: `Bearer ${cookieStore.get("access-token")?.value}`,
-      },
-    }
-  ).then((res) => res.json());
-  console.log(userDetail);
 
-  if (!userDetail.id) {
+// export const dynamic = "force-dynamic";
+
+const AuthorizeUserAction = async () => {
+  const cookieStore = cookies();
+  const token = (await cookieStore).get("access-token")?.value;
+
+  if (!token) {
     return <UserAccessMenu />;
   }
 
-  const user: IResponseUserData = await fetch(
-    `http://localhost:3000/api/user/${userDetail.id}`
-  ).then((res) => res.json());
+  const meRes = await fetch("http://localhost:8000/api/users/me", {
+    headers: { Authorization: `Bearer ${token}` },
+    cache: "no-store",
+  });
 
-  return (
-    <div>
-      <UserAccessMenu loginData={user} />
-    </div>
-  );
+  if (!meRes.ok) {
+    return <UserAccessMenu />;
+  }
+
+  const me: { id?: string; role?: string } = await meRes.json();
+  if (!me?.id) {
+    return <UserAccessMenu />;
+  }
+
+  const userRes = await fetch(`http://localhost:8000/api/users/${me.id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+    cache: "no-store",
+  });
+
+  if (!userRes.ok) {
+    return <UserAccessMenu />;
+  }
+
+  const user: IResponseUserData = await userRes.json();
+
+  return <UserAccessMenu loginData={user} />;
 };
 
 export default AuthorizeUserAction;
