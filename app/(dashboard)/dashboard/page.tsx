f42e6f258c9e55ed4692261016ab6d872f6d007b
mohammad <137170507+orca0016@users.noreply.github.com>
2025-08-14 21:08:41 +0330

    Revert "Feature/entity management product (#9)"
    This reverts commit 2137eca19e7835908bafd2f619e846cd592012be.

--- app/(dashboard)/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ShopIcon } from "@/components/atoms/icons"
-
-const DashboardPage = () => {
-  return (
-    <div className="h-[120vh] text-custom-purple">صفحه اصلی <ShopIcon/></div>
-  )
-}
-
-export default DashboardPage
